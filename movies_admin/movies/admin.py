74fdf9568a52cd0d7859de2e90122e6104c395f4
Sergey Ognev <ognev.s.d@gmail.com>
2024-06-16 21:49:24 +0300

    Add admin panel setup

---
+++ movies_admin/movies/admin.py
@@ -0,0 +1,42 @@
+from django.contrib import admin
+from .models import Genre, Filmwork, GenreFilmwork, Person, PersonFilmwork
+
+
+class GenreFilmworkInline(admin.TabularInline):
+    model = GenreFilmwork
+
+
+class PersonFilmworkInline(admin.TabularInline):
+    model = PersonFilmwork
+
+
+# Register your models here.
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    # Поиск по полям
+    search_fields = ("name",)
+
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    # Поиск по полям
+    search_fields = ("full_name",)
+
+
+@admin.register(Filmwork)
+class FilmworkAdmin(admin.ModelAdmin):
+    inlines = (
+        GenreFilmworkInline,
+        PersonFilmworkInline,
+    )
+    # Отображение полей в списке
+    list_display = (
+        "title",
+        "type",
+        "creation_date",
+        "rating",
+    )
+    # Фильтрация в списке
+    list_filter = ("type", "creation_date")
+    # Поиск по полям
+    search_fields = ("title", "description", "id")
