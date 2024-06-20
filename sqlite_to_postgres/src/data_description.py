b304b1e378b6319f8ffffa0d2fe7d42923bd879f
Sergey Ognev <ognev.s.d@gmail.com>
2024-06-20 11:42:30 +0300

    Describe all database tables

--- sqlite_to_postgres/src/data_description.py
+++ sqlite_to_postgres/src/data_description.py
@@ -7,24 +7,54 @@ from dataclasses import dataclass, fields
 class DatabaseItem:
     id: uuid.UUID
 
-    def to_csv(self):
-        values = []
-        for field in fields(self):
-            item = getattr(self, field.name)
-            if item is None:
-                values.append("")
-            else:
-                values.append(str(item))
-        return "\t".join(values) + "\n"
+    def generate_col_count(self) -> str:
+        column_names = [field.name for field in fields(self)]
+        col_count = ", ".join(["%s"] * len(column_names))
+        return col_count
+
+    def generate_col_names_str(self) -> str:
+        column_names = [field.name for field in fields(self)]  # [id, name]
+        column_names_str = ",".join(column_names)
+        return column_names_str
 
 
 @dataclass(frozen=True)
-class Filmwork(DatabaseItem):
+class Timestamped:
+    created: datetime.datetime
+    modified: datetime.datetime
+
+
+@dataclass(frozen=True)
+class Filmwork(DatabaseItem, Timestamped):
     title: str
     description: str
     creation_date: datetime.datetime
     rating: float
     type: str
     file_path: str
+
+
+@dataclass(frozen=True)
+class Genre(DatabaseItem, Timestamped):
+    name: str
+    description: str
+
+
+@dataclass(frozen=True)
+class Person(DatabaseItem, Timestamped):
+    full_name: str
+
+
+@dataclass(frozen=True)
+class GenreFilmWork(DatabaseItem):
+    film_work_id: uuid.UUID
+    genre_id: uuid.UUID
     created: datetime.datetime
-    modified: datetime.datetime
+
+
+@dataclass(frozen=True)
+class PersonFilmWork(DatabaseItem):
+    film_work_id: uuid.UUID
+    person_id: uuid.UUID
+    role: str
+    created: datetime.datetime
